cca50544d53802fb8a5f9b81a6834360476dfd13
siva shunmuga sundaram <sivashunmugasundaram007@gmail.com>
2024-02-17 12:20:20 +0530

    Edge Case For the Invalid Input Cases

--- src/Calculator.java
+++ src/Calculator.java
@@ -1,5 +1,8 @@
 package src;
 
+import javax.swing.*;
+import java.security.spec.ECFieldF2m;
+
 public class Calculator {
 
     //base case
@@ -7,44 +10,65 @@ public class Calculator {
 
     //Input: "", "1", "1,5"
     //Output: 0, 1, 6
-    protected int add(String number){
-        //solving the initing base case that is empty
+    protected int add(String number) {
+        //variable for storing the sum of values in String
+        int sum = 0;
+        try {
+            //solving the initing base case that is empty
 
-        //intput : "" output : 0
-        if(number==null|| number.isEmpty()){
-            return 0;
-        }
+            //intput : "" output : 0
+            if (number == null || number.isEmpty()) {
+                return 0;
+            }
 
-    //solving one or more integer in a String.
-        //split the String with the comma as delimmitter
+            //solving one or more integer in a String.
+            //split the String with the comma as delimmitter
 
-        //Allow the add method to handle new lines between numbers (instead of commas).
+            //Allow the add method to handle new lines between numbers (instead of commas).
 //The following input is valid: "1\n2,3" (will return 6)
 //    The following input is invalid: "1,\n"
 
 
-        String num[]=number.split("[\n,]");
+            String num[] = number.split("[\n,]");
 //        we can use the multiple Delimetter Also , like [,?//n?/n]
-        //variable for storing the sum of values in String
-        int sum=0;
-        //case 1:
+
+            //case 1:
             //if input has one number (ie) input : "1" output: 1
-            if(num.length==1)
+            if (num.length == 1)
                 return Integer.parseInt(num[0]);
 
-            for(String iter:num){
-                if( iter.contains("\\n") || iter.contains("\n")){
-                    String temp[]=iter.split("\\\\n");
-                    for(String tempNumber:temp){
-                        sum+=Integer.parseInt(tempNumber);
+            for (String iter : num) {
+                if (iter.contains("\\n") || iter.contains("\n")) {
+                    String temp[] = iter.split("\\\\n");
+                    for (String tempNumber : temp) {
+                        if (tempNumber.isEmpty()) {
+                            throw new InvalidInput();
+                        }
+                        sum += Integer.parseInt(tempNumber);
                     }
                     continue;
                 }
-                sum+=Integer.parseInt(iter);
+                sum += Integer.parseInt(iter);
             }
 
             return sum;
 
+        } catch (NumberFormatException e) {
+            throw new RuntimeException(e);
+        }
+        catch (InvalidInput e){
+            //case for the invalid Input
+        }
+        finally {
+            return sum;
+        }
     }
 
+}
+
+class InvalidInput extends Exception {
+    @Override
+    public String toString() {
+        return "Invalid Input";
+    }
 }
